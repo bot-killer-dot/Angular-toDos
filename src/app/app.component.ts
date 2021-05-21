10cab4c2c043fd8c4c016b2fe80489efbc623717
hamayun <hamayun.waheed1999@gmail.com>
2021-05-21 20:14:42 +0500

    complete the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-ang-app';
+  todoArray: string[] = [];
+  addTodo(value: string){
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+  deleteItem(todo: string){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+      if(todo== this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
 }
